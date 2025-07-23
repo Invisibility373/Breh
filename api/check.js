e54ec324ae4217f756ee54527006ca345bcd0840
Invisibility373 <invisibility373@gmail.com>
2025-07-23 00:47:08 -0700

    Update check.js

--- api/check.js
+++ api/check.js
@@ -15,7 +15,6 @@ export default async function handler(req, res) {
   try {
     const files = await fs.promises.readdir(dir);
 
-    // Filter files for this username
     const userTriggers = files
       .filter(f => f.startsWith(username + '-'))
       .map(f => f.replace(username + '-', '').replace('.txt', ''));
@@ -26,6 +25,7 @@ export default async function handler(req, res) {
 
     res.status(200).json({ exists: true, triggerIds: userTriggers });
   } catch (err) {
+    console.error(err);
     res.status(500).send('Server error');
   }
 }
